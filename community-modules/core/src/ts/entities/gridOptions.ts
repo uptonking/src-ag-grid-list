24bb20c51f3740a4df715b821e7ebf9261556bbd
uptonking <jinyaoo86@gmail.com>
2020-09-25 22:03:06 +0800

    chore commit

--- community-modules/core/src/ts/entities/gridOptions.ts
+++ community-modules/core/src/ts/entities/gridOptions.ts
@@ -323,7 +323,8 @@ export interface GridOptions {
 
   //This is an array of ExcelStyle, but because that class lives on the enterprise project is referenced as any from the client project
   excelStyles?: any[];
-  /** @deprecated Use floatingFilter on the colDef instead */ floatingFilter?: boolean;
+  /** @deprecated Use floatingFilter on the colDef instead */
+  floatingFilter?: boolean;
   suppressExcelExport?: boolean;
   suppressCsvExport?: boolean;
 
